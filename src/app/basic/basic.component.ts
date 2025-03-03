dc24dc0abb588cbc085192db894b97dc592105b6
agm9581 <agm9581@gmail.com>
2025-03-04 00:48:03 +0100

    Subscription and subscribe method and callbacks

---
+++ src/app/basic/basic.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { from, of, Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-basic',
+  templateUrl: './basic.component.html',
+  styleUrls: ['./basic.component.scss'],
+})
+export class BasicComponent implements OnInit, OnDestroy {
+  sub!: Subscription;
+  subArray!: Subscription;
+  subFrom!: Subscription;
+  ngOnInit(): void {
+    //subscribe will pass the value receive as a whole, in this case 2, then 4 and then 6
+    this.sub = of(2, 4, 6).subscribe((item) =>
+      console.log('Value from of:', item)
+    );
+    //subscribe in this case will pass the whole array
+    this.subArray = of([2, 4, 6]).subscribe((item) =>
+      console.log('Value from of array:', item)
+    );
+
+    // from handles the observable knowing the existence of the array and will split each of the values
+    this.subFrom = from([20, 15, 10, 5]).subscribe({
+      next: (item) => console.log('From item:', item),
+      error: (err) => console.log('From error:', err),
+      complete: () => console.log('From complete'),
+    });
+
+    // this is the general callback functions for the subscribe method, this enables the data to process, still it can be handled without the object typing
+    this.subFrom = from([20, 15, 10, 5]).subscribe({
+      next: (item) => console.log('From item:', item),
+      error: (err) => console.log('From error:', err),
+      complete: () => console.log('From complete'),
+    });
+
+    // this is deprecated but also valid
+    this.subFrom = from([1, 2, 3, 5]).subscribe(
+      (item) => {
+        return item;
+      },
+      (err) => {
+        return err;
+      },
+      () => {
+        return 'complete';
+      }
+    );
+  }
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+    this.subArray.unsubscribe();
+    this.subFrom.unsubscribe();
+  }
+}
